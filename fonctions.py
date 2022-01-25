9aad89201a10dabbbf0776d3248a097aaaaf73c5
issam-hamidi <97039642+issam-hamidi@users.noreply.github.com>
2022-01-25 12:00:18 +0100

    Add files via upload

---
+++ fonctions.py
@@ -0,0 +1,54 @@
+from math import sqrt
+
+def pourcentage(a,b):
+    '''
+    :param a: partie de b
+    :param b: total
+    :return: Pourcentage de a par rapport à b
+    '''
+    return a/(b/100)
+
+
+
+def moyenne(liste):
+    '''
+    :param liste: liste contenant des valeurs  entières ou réelles
+    :return: La moyenne des valeurs contenus dans la liste
+    '''
+    total = 0
+    for element in liste:
+        total += element
+    return total/len(liste)
+
+
+def ecart_type(liste, moyenne):
+    '''
+    :param liste: Liste de valeurs
+    :param moyenne: Valeur moyenne de la liste de valeurs
+    :return: Retourne l'écart-type
+    '''
+    variance = 0
+    for element in liste:
+        variance += (element - moyenne)**2
+    variance /= len(liste)
+    return sqrt(variance)
+
+def covariance(liste_x, liste_y, moyenne_x, moyenne_y):
+    resultat = 0
+    taille = len(liste_x)
+    for i in range(taille):
+        resultat += (liste_x[i] - moyenne_x) * (liste_y[i] - moyenne_y)
+    resultat /= taille
+    return resultat
+
+def coefficient_correlation(liste_x, liste_y, moyenne_x, moyenne_y):
+    return covariance(liste_x, liste_y, moyenne_x, moyenne_y) * 1/(ecart_type(liste_x, moyenne_x) * ecart_type(liste_y, moyenne_y))
+
+
+
+
+
+
+
+
+
---
+++ interpretation_donnees.py
@@ -0,0 +1,97 @@
+import fonctions as fc
+
+
+
+'''
+============================================ PARTIE VELO===================================================
+'''
+
+def extraction_moyennes_velos(fichier):
+    '''
+    :param fichier: fichier à partir duquel on souhaite extraire les données
+    :return: Retourne la liste des Moyennes du taux d'occupation de tous les parkings vélo à intervalle de temps défini
+    '''
+    fichier = open(fichier, "r", encoding="utf-8")
+    lignes = fichier.readlines()
+    fichier.close()
+
+    parkings = []
+    for ligne in lignes:
+        ligne = ligne.strip()
+        ligne = ligne.strip("|")
+        ligne = ligne.split("|")
+        parkings.append(ligne)
+
+    nombre_parkings = len(parkings)
+    nombre_prelevements = len(parkings[0])  # correspond au nombre de prélèvements effectués
+    moyennes = []
+
+    for i in range(nombre_prelevements):
+        pourcentages = []
+        for j in range(nombre_parkings):
+            parking = parkings[j][i]
+            parking = parking.strip(";")
+            parking = parking.split(";")
+            nom, dispo, de, jusqua = [element.split("=")[1] for element in parking]
+            pourcentages.append(fc.pourcentage(int(dispo), int(jusqua)))
+        moyennes.append(fc.moyenne(pourcentages))
+    return moyennes
+
+
+'''
+=============================================== PARTIE VOITURE ======================================================
+'''
+
+def extraction_moyennes_voitures(fichier):
+    '''
+    :param fichier: fichier à partir duquel on souhaite extraire les données
+    :return: Retourne la liste des Moyennes du taux d'occupation de tous les parkings voiture à intervalle de temps défini
+    '''
+    fichier = open(fichier, "r", encoding="utf-8")
+    lignes = fichier.readlines()
+    fichier.close()
+
+    parkings = []
+    for ligne in lignes:
+        ligne = ligne.strip()
+        ligne = ligne.strip("|")
+        ligne = ligne.split("|")
+        parkings.append(ligne)
+
+    nombre_parkings = len(parkings)
+    nombre_prelevements = len(parkings[0])  # correspond au nombre de prélèvements effectués
+    moyennes = []
+
+    for i in range(nombre_prelevements):
+        pourcentages = []
+        for j in range(nombre_parkings):
+            parking = parkings[j][i]
+            parking = parking.strip(";")
+            parking = parking.split(";")
+            date, nom, statut, libres, total = [element.split("=")[1] for element in parking]
+            pourcentages.append(fc.pourcentage(int(libres), int(total)))
+        moyennes.append(fc.moyenne(pourcentages))
+    return moyennes
+
+
+
+
+
+moyennes_velos = extraction_moyennes_velos("velos.txt")
+moyennes_voitures = extraction_moyennes_voitures("voitures.txt")
+
+moyenne_totale_velos = fc.moyenne(moyennes_velos)
+moyenne_totale_voitures = fc.moyenne(moyennes_voitures)
+
+
+with open("donnees_courbe.dat", "w", encoding="utf-8") as fichier:
+    for i in range(12+1):
+        fichier.write("{} {} {} {}\n".format(i*30, moyennes_velos[i], i*30, moyennes_voitures[i]))
+    fichier.close()
+
+
+#print(fc.coefficient_correlation(moyennes_velos, moyennes_voitures, moyenne_totale_velos, moyenne_totale_voitures))
+
+
+
+
